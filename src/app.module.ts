6e44d480e08f98bdda62acdbce3bdcd58c385e49
Antares <antares.mi@outlook.com>
2024-09-25 19:26:53 +0800

    add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { forwardRef, MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 
 import { ConfigModule } from "@/config/config.module";
+import { DatabaseModule } from "@/database/database.module";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { RedisModule } from "./redis/redis.module";
 import { ViewMiddleware } from "./view.middleware";
 
 @Module({
-    imports: [ConfigModule],
+    imports: [ConfigModule, forwardRef(() => DatabaseModule), RedisModule],
     controllers: [AppController],
     providers: [AppService],
 })
