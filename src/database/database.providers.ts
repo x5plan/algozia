7b6212e5dbfd51d69cbb91e25c96dc9c533faa98
Antares <antares.mi@outlook.com>
2024-09-25 19:30:14 +0800

    Integrate TypeORM and Redis (#2)

---
+++ src/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { isProduction } from "@/common/utils/env";
+import { ConfigService } from "@/config/config.service";
+
+export const databaseProviders = [
+    TypeOrmModule.forRootAsync({
+        useFactory: (configService: ConfigService) => ({
+            host: configService.config.database.hostname,
+            port: configService.config.database.port,
+            username: configService.config.database.username,
+            password: configService.config.database.password,
+            database: configService.config.database.database,
+            entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+            logging: !isProduction(),
+            synchronize: true,
+            charset: "utf8mb4",
+        }),
+        inject: [ConfigService],
+    }),
+];
