16bd6f1a1ba6bf86c56de5444f0e221db94b3074
Antares <antares.mi@outlook.com>
2024-10-08 21:48:36 +0800

    fix

--- src/problem/problem-judge-info.entity.ts
+++ src/problem/problem-judge-info.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
 import { ToEntity } from "@/common/types/to-entity";
 
 import { ProblemEntity } from "./problem.entity";
-import { E_ProblemType, IProblemJudgeInfoEditable } from "./problem.type";
+import { E_ProblemType, IProblemJudgeInfo, IProblemJudgeInfoEditable } from "./problem.type";
 
 @Entity("problem_judge_info")
 export class ProblemJudgeInfoEntity implements ToEntity<IProblemJudgeInfoEditable> {
@@ -17,21 +17,6 @@ export class ProblemJudgeInfoEntity implements ToEntity<IProblemJudgeInfoEditabl
     @Column({ name: "type", type: "enum", enum: E_ProblemType })
     public type: E_ProblemType;
 
-    @Column({ name: "time_limit", type: "integer" })
-    public timeLimit: number;
-
-    @Column({ name: "memory_limit", type: "integer" })
-    public memoryLimit: number;
-
-    @Column({ name: "file_io", type: "boolean", nullable: true })
-    public fileIO: boolean | null;
-
-    @Column({ name: "input_file_name", type: "text", nullable: true })
-    public inputFileName: string | null;
-
-    @Column({ name: "output_file_name", type: "text", nullable: true })
-    public outputFileName: string | null;
-
-    @Column({ name: "testdata_config", type: "simple-json", nullable: true })
-    public testdataConfig: unknown | null;
+    @Column({ type: "json" })
+    public judgeInfo: IProblemJudgeInfo;
 }
