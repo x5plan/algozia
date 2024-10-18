44f423e6f662ab80cbbaf264cb9d8ccfd5b5d409
Antares <antares.mi@outlook.com>
2024-10-18 15:09:19 +0800

    fix

--- src/permission/permission.entity.ts
+++ src/permission/permission.entity.ts
@@ -13,9 +13,9 @@ export class PermissionEntity {
     @PrimaryColumn({ name: "user_id" })
     public userId: number;
 
-    @Column({ name: "allowed_problem_ids", type: "simple-array", nullable: false })
+    @Column({ name: "allowed_problem_ids", type: "json", nullable: false })
     public allowedProblemIds: number[];
 
-    @Column({ name: "allowed_contest_ids", type: "simple-array", nullable: false })
+    @Column({ name: "allowed_contest_ids", type: "json", nullable: false })
     public allowedContestIds: number[];
 }
