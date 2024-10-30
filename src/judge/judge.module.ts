65ebdf8bbb6ee7bbe851c3e1a77447166ed2a490
Antares <antares.mi@outlook.com>
2024-10-30 14:57:56 +0800

    update

--- src/judge/judge.module.ts
+++ src/judge/judge.module.ts
@@ -1,14 +1,21 @@
 import { forwardRef, Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { RedisModule } from "@/redis/redis.module";
 import { SubmissionModule } from "@/submission/submission.module";
 
 import { JudgeGateway } from "./judge.gateway";
+import { JudgeClientEntity } from "./judge-client.entity";
+import { JudgeClientService } from "./judge-client.service";
 import { JudgeQueueService } from "./judge-queue.service";
 
 @Module({
-    imports: [forwardRef(() => SubmissionModule), forwardRef(() => RedisModule)],
-    providers: [JudgeQueueService, JudgeGateway],
-    exports: [JudgeQueueService, JudgeGateway],
+    imports: [
+        TypeOrmModule.forFeature([JudgeClientEntity]),
+        forwardRef(() => SubmissionModule),
+        forwardRef(() => RedisModule),
+    ],
+    providers: [JudgeClientService, JudgeQueueService, JudgeGateway],
+    exports: [JudgeClientService, JudgeQueueService, JudgeGateway],
 })
 export class JudgeModule {}
